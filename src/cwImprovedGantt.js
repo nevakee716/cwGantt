1b211b129d9a048242282ebf54781d4d6e730189
nevakee716 <nevakee716@gmail.com>
2018-08-27 18:18:12 +0200

    taking configuration from evolve

--- src/cwImprovedGantt.js
+++ src/cwImprovedGantt.js
@@ -24,67 +24,8 @@
         this.objectTypeTable = {};
         this.reverseobjectTypeTable = {};
         this.removedNodes = [];
-        this.config = {
-            "nodes": [{
-                "nodeID": "projet_474185792",
-                "startDate": "STARTDATE",
-                "endDate": "ENDDATE",
-                "completion": "PERCENTCOMPLETE",
-                "finishToStart": [],
-                "startToStart": [],
-                "finishToFinish": [],
-                "milestone" : [],
-                "classStyle": "ggroupblack"
-            }, {
-                "nodeID": "projet_20027_205402790",
-                "startDate": "STARTDATE",
-                "endDate": "ENDDATE",
-                "completion": "PERCENTCOMPLETE",
-                "finishToStart": ["projet_20217_39247427"],
-                "startToStart": ["projet_20218_858571321"],
-                "finishToFinish": ["projet_20219_1725533778"],
-                "milestone" : ["projet_20027_2109559671"],
-                "classStyle": "gtaskred"
-            }],
-            "hiddenNodes": [""],
-            "complementaryNode": [],
-            "dateDisplayFormat": "mm/dd/yyyy",
-            "scale": "month",
-            "fixedColumnMap": {
-                "startdate": {
-                    "hide":false,
-                    "scriptname": "startdate",
-                    "width": "70"
-                },
-                "enddate": {
-                    "hide":false,
-                    "scriptname": "enddate",
-                    "width": "70"
-                },
-                "duration": {
-                    "hide":false,
-                    "scriptname": "duration",
-                    "width": "70"
-                },
-                "pccomplete": {
-                    "hide":true,
-                    "scriptname": "pccomplete",
-                    "width": "50"
-                },
-                "taskname": {
-                    "hide":false,
-                    "scriptname": "taskname",
-                    "width": "400"
-                }
-            },
-            "customColumnMap": [{
-                "scriptname": "type",
-                "label": "Category",
-                "width": "70"
-            }]
-
-        };
-
+        this.config = JSON.parse(this.options.CustomOptions['JsonConfiguration']);
+       
         var nodesObj = {};
         this.config.nodes.forEach(function(n) {
             nodesObj[n.nodeID] = n;
